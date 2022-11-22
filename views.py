23a63fb0cf1807e480d61446a64b5c0c7650ee14
Eliseo Mariano Amaya <84360137+mar19a@users.noreply.github.com>
2022-11-22 01:30:08 -0500

    Add files via upload

---
+++ views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Project
+from .forms import ProjectForm
+def projects(request):
+    projects = Project.objects.all()
+    context = {'projects' : projects}
+    #'page' : page is connected to projects>views.py>
+    return render(request, 'projects/projects.html', context)
+    #Template rendering for templates > project.html
+def project(request, pk):
+    projectObj = Project.objects.get(id=pk)
+    return render(request, 'projects/single-project.html', {'project': projectObj})
+
+def createProject(request):
+    form = ProjectForm()
+    context = {'form': form }
+    return render(request, 'projects/project_form.html', context)
